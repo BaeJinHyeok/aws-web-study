d54fdb089527e0ed3e53adcbea30dc31716ada1f
Jh <gumemoge@gmail.com>
2023-07-25 17:55:24 +0900

    study

---
+++ src/main/java/com/study/awswebstudy/AwsWebStudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.awswebstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsWebStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsWebStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/awswebstudy/AwsWebStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.awswebstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsWebStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
