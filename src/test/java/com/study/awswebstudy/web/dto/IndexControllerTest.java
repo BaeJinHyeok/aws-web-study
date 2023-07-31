071bafe8777ca5d9336002ee3ba7237fdeca2818
Jh <gumemoge@gmail.com>
2023-08-01 02:33:22 +0900

    mustache start

---
+++ src/test/java/com/study/awswebstudy/web/dto/IndexControllerTest.java
@@ -0,0 +1,30 @@
+package com.study.awswebstudy.web.dto;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    //Execution failed for task ':test'. Error 발생 > 해결방법 > File -> Setting -> Bulid ... -> Gradel -> Run tests using 을 기존 Gradle 에서 Intellij IDEA 로 바꿔줌
+    @Test
+    void 메인페이지_로딩() {
+        //when
+        String body = this.testRestTemplate.getForObject("/", String.class);
+
+        //then
+        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+    }
+}
