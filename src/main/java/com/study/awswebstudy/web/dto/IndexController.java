2bdafdf0013b66943b784dbd1ea5aeb1b9543d82
Jh <gumemoge@gmail.com>
2023-08-05 21:49:32 +0900

    delete ing..

--- src/main/java/com/study/awswebstudy/web/dto/IndexController.java
+++ src/main/java/com/study/awswebstudy/web/dto/IndexController.java
@@ -1,10 +1,12 @@
 package com.study.awswebstudy.web.dto;
 
+import com.study.awswebstudy.domain.posts.PostsRepository;
 import com.study.awswebstudy.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -16,12 +18,19 @@ public class IndexController {
 //        return "index";
 //    }
 
+    private final PostsService postsService;
+
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
     }
 
-    private final PostsService postsService;
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("posts", dto);
+        return "posts-update";
+    }
 
     @GetMapping("/")
     public String index(Model model) { //Model -> 서버템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음. 여기서는 postsService.finAllDesc()로 가져온 결과를 posts로 index.mustache 에 전달함
--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -6,6 +6,12 @@ var main = {
         $('#btn-save').on('click', function () {
             _this.save();
         });
+        $('#btn-update').on('click', function () { // btn-update란 id 를 가진 HTML 엘리멘트에 click 이벤트가 발생할때 update function 을 실행하도록 이벤트를 등록.
+            _this.update();
+        });
+        $('#btn-delete').on('click', function () {
+            _this.delete();
+        });
     },
     save: function () {
         var data = {
@@ -22,6 +28,57 @@ var main = {
             data: JSON.stringify(data)
         }).done(function () {
             alert('글이 등록되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+
+    update: function () {
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT', // 여러 HTTP 메소드 중 PUT 메소드를 선택 -> PostsApiController에 있는 API에서 이미 @PutMapping 으로 선언했기 때문에 PUT을 사용해야함
+                         // 참고로 이는 PEST 규약에 맞게 설정된 것임.
+                         // REST에서 CRUD는 다음과 같이 HTTP Method 에 매핑됨.
+                         // 생성 CREATE - POST ,
+                         // 읽기 READ - GET ,
+                         // 수정 UPDATE - PUT ,
+                         // 삭제 DELETE - DELETE
+                         // 버튼 추가 생성시 -> 버튼 이벤트 등록 구현, function 구현 후 전체목록에서 수정 페이지로 이동할 수 있게 기능추가 한 다음 IndexController 에 update 메소드 추가.
+            url: '/api/v1/posts/'+id, // 어느 게시글을 수정할지 *URL Path로 구분* 하기 위해 Path에 id 를 추가함!
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function () {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    delete: function () {
+        var data = {
+            title: $('#title').val(),
+            author: $('#author').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function () {
+            alert('글이 삭제되었습니다.');
             window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error));
