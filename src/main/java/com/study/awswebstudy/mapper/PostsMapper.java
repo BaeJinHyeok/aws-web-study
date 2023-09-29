62bc5ff6271f14aa93f62d54f60700360d6ecfa5
Jh <gumemoge@gmail.com>
2023-09-29 17:24:55 +0900

    refactoring..

--- src/main/java/com/study/awswebstudy/mapper/PostsMapper.java
+++ src/main/java/com/study/awswebstudy/mapper/PostsMapper.java
@@ -1,5 +1,9 @@
 package com.study.awswebstudy.mapper;
 
+import com.study.awswebstudy.web.dto.PostsSaveRequestDto;
+
 @Mapper
 public interface PostsMapper {
+
+    long save(PostsSaveRequestDto requestDto) throws Exception;
 }
