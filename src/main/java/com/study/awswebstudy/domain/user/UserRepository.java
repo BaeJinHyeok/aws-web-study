6391fd8a335ffd7e7055137e1c2bf27db9d562b1
Jh <gumemoge@gmail.com>
2023-10-08 17:09:26 +0900

    paging

--- src/main/java/com/study/awswebstudy/domain/user/UserRepository.java
+++ src/main/java/com/study/awswebstudy/domain/user/UserRepository.java
@@ -1,5 +1,8 @@
 package com.study.awswebstudy.domain.user;
 
+import com.study.awswebstudy.domain.posts.Posts;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -7,4 +10,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<Users, Long> {
 
     Optional<Users> findByEmail(String email); // 소셜 로그인으로 반환되는 값 중 email을 통해 이미 생성된 사용자인지 처음 가입하는 사용자인지 판단하기 위한 메소드임.
+    Page<Posts> findByUserOrderByIdDesc(Users user, Pageable pageable);
 }
