f2c6303c9b19da1230f528a15b4dacc053641eb0
Jh <gumemoge@gmail.com>
2023-10-17 23:59:24 +0900

    feat: authinterceptor

---
+++ src/main/java/com/study/awswebstudy/config/AuthInterceptor.java
@@ -0,0 +1,9 @@
+package com.study.awswebstudy.config;
+
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class AuthInterceptor {
+
+    public static AuthInterceptor (HttpRequest request, HttpResponse response)
+}
