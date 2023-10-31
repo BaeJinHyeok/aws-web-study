f9472e44667f45f15d257c0fd62fdb9887ea723f
Jh <gumemoge@gmail.com>
2023-11-01 00:02:02 +0900

    feat: AccessLog postHandle

--- src/main/java/com/study/awswebstudy/Interceptor/Interceptor.java
+++ src/main/java/com/study/awswebstudy/Interceptor/Interceptor.java
@@ -1,10 +1,15 @@
 package com.study.awswebstudy.Interceptor;
 
 import com.study.awswebstudy.service.posts.AccessLogService;
+import com.study.awswebstudy.web.dto.AccessLogDto;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.lang.reflect.Method;
 
 @RequiredArgsConstructor
 public class Interceptor implements HandlerInterceptor {
@@ -16,6 +21,35 @@ public class Interceptor implements HandlerInterceptor {
         return true;
     }
 
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        String requestUri = request.getRequestURI();
+
+
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod handlerMethod = (HandlerMethod) handler;
+            Method method = handlerMethod.getMethod();
+
+            Class<?> clazz = method.getDeclaringClass();
+
+            String className = clazz.getName();
+            String classSimpleName = clazz.getSimpleName();
+            String methodName = method.getName();
+
+            AccessLogDto accessLog = new AccessLogDto();
+
+            accessLog.setRequestUri(requestUri);
+
+            accessLog.setClassName(className);
+            accessLog.setClassSimpleName(classSimpleName);
+            accessLog.setMethodName(methodName);
+
+            accessLogService.register(accessLog);
+        } else {
+            System.out.println("Handler" + handler);
+        }
+
+    }
 
 
 }
