a140a777d673193eda72f602aaec3e6ba788b06c
Jh <gumemoge@gmail.com>
2023-11-03 01:34:23 +0900

    feat: AccessLog Mappers

--- src/main/java/com/study/awswebstudy/mapper/AccessLogMapper.java
+++ src/main/java/com/study/awswebstudy/mapper/AccessLogMapper.java
@@ -4,8 +4,8 @@ import com.study.awswebstudy.web.dto.AccessLogDto;
 
 import java.util.List;
 
-public class AccessLogMapper {
-
+@Mapper
+public interface AccessLogMapper {
     void create(AccessLogDto accessLogDto);
 
     List<AccessLogDto> list();
