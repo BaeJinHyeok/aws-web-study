899c23d8eebf4b05745f5936aa79c3b20570c7b8
Jh <gumemoge@gmail.com>
2023-11-20 00:20:44 +0900

    feat: accessLog service, inteceptor config

---
+++ src/main/java/com/study/awswebstudy/config/auth/InterceptorConfig.java
@@ -0,0 +1,15 @@
+package com.study.awswebstudy.config.auth;
+
+import com.study.awswebstudy.Interceptor.Interceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+
+@RequiredArgsConstructor
+public class InterceptorConfig {
+
+    private final accessLogservice
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(new Interceptor(accessLogService)).addPathPatterns("/**")
+    }
+}
