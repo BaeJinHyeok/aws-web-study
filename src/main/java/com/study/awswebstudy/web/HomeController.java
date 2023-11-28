517e25a13344042a2daf742f766d236f3daa01c5
Jh <gumemoge@gmail.com>
2023-11-29 00:01:31 +0900

    fix: HomeController

--- src/main/java/com/study/awswebstudy/web/HomeController.java
+++ src/main/java/com/study/awswebstudy/web/HomeController.java
@@ -1,18 +1,12 @@
 package com.study.awswebstudy.web;
 
 import com.study.awswebstudy.web.dto.AccessLogDto;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.lang.reflect.Method;
-import java.net.http.HttpRequest;
-import java.util.Map;
-
-public class AccessLogController {
+public class HomeController {
     @GetMapping("/")
     public AccessLogDto createAccessLog(HttpServletRequest request){
 
-        String method = request.getMethod();
     }
 }
