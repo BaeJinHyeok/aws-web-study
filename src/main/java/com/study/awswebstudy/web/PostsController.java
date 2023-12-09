3fe79dc7909e68112d9dbb462957b4a617ad0167
Jh <gumemoge@gmail.com>
2023-12-09 23:58:36 +0900

    feat : detail view post

--- src/main/java/com/study/awswebstudy/web/PostsController.java
+++ src/main/java/com/study/awswebstudy/web/PostsController.java
@@ -1,4 +1,16 @@
 package com.study.awswebstudy.web;
 
+import com.study.awswebstudy.web.dto.PostsResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 public class PostsController {
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable long id,
+                                      @RequestParam(value =  "st", required = false, defaultValue = "1") Integer offset,
+                                      @RequestParam (value = "ed", required = false, defaultValue =  "10") Integer limit){
+        return postsService.findById(id);
+    }
 }
