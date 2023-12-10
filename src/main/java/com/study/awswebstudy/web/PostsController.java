cd5e5742d0eaab4fc766c42945cb31130328ac54
Jh <gumemoge@gmail.com>
2023-12-10 23:58:39 +0900

    feat : page

--- src/main/java/com/study/awswebstudy/web/PostsController.java
+++ src/main/java/com/study/awswebstudy/web/PostsController.java
@@ -1,16 +1,21 @@
 package com.study.awswebstudy.web;
 
+import com.study.awswebstudy.domain.Pages.Pages;
 import com.study.awswebstudy.web.dto.PostsResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public class PostsController {
-
+    final private Pages pages
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById (@PathVariable long id,
                                       @RequestParam(value =  "st", required = false, defaultValue = "1") Integer offset,
                                       @RequestParam (value = "ed", required = false, defaultValue =  "10") Integer limit){
+
+        offset = pages.setStart();
+        limit = pages.setLimit();
+        int total = pages.setTotal();
         return .findById(id);
     }
 }
